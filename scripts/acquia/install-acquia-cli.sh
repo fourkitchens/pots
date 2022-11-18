5124a3b5f60e0ec0c04b6372bb700266b8050974
Allan Chappell <generalredneck@gmail.com>
2022-11-18 10:38:15 -0600

    fix: checking for PHP 8 and getting the correct acli version

--- scripts/acquia/install-acquia-cli.sh
+++ scripts/acquia/install-acquia-cli.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
+
+# Functions checking if version is less than another in bash.
+# @see https://stackoverflow.com/a/4024263/658023
+verlte() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+verlt() {
+    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
 echo '\nPulling down and installing Acquia CLI to vendor/bin...'
 cd vendor/bin
-curl -Lo acli https://github.com/acquia/cli/releases/download/1.30.1/acli.phar
+PHP_VERSION=$(php -r "echo PHP_VERSION;")
+# If php is Less than 8, install the PHP 7 version of acli
+if [[ verlt "$PHP_VERSION" "8.0.0" ]]; then
+  curl -Lo acli https://github.com/acquia/cli/releases/download/1.30.1/acli.phar
+else
+  curl -Lo acli https://github.com/acquia/cli/releases/latest/download/acli.phar
+fi
+
 chmod +x acli
