6727af2da517c8dcb1741ea238c044b305e24d06
Allan Chappell <generalredneck@gmail.com>
2024-08-07 16:12:56 -0500

    merge in pots

--- scripts/acquia/install-acquia-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo '\nPulling down and installing Acquia CLI to vendor/bin...'
-cd vendor/bin
-curl -Lo acli https://github.com/acquia/cli/releases/download/1.30.1/acli.phar
-chmod +x acli
